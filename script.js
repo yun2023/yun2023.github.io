7dc2ca8f186673260cff38569ecaa2f507f872c0
yun2023 <119715515+yun2023@users.noreply.github.com>
2023-11-13 21:36:29 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function mostrarRespuesta(acepto) {
+    var respuesta = document.getElementById("respuesta");
+    respuesta.style.display = "block";
+  
+    if (acepto) {
+      respuesta.innerHTML = "<p>¡Has hecho muy feliz a alguien hoy!</p><img src='corazon.png' alt='Corazón'>";
+    }
+  }
+  
+  function moverBotonNo() {
+    var btnNo = document.getElementById("btnNo");
+    var xPos = Math.floor(Math.random() * window.innerWidth - btnNo.clientWidth);
+    var yPos = Math.floor(Math.random() * window.innerHeight - btnNo.clientHeight);
+    btnNo.style.position = "absolute";
+    btnNo.style.left = xPos + "px";
+    btnNo.style.top = yPos + "px";
+  }
